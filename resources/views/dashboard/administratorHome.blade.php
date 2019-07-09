79828e2a6b03ba616396236e980eb5e4e95eced1
KiddoKidz <mzuhdi.zamrud@gmail.com>
2019-07-09 17:43:37 +0700

    fixing youtube height & carousel height

--- resources/views/dashboard/administratorHome.blade.php
+++ resources/views/dashboard/administratorHome.blade.php
@@ -65,7 +65,7 @@
       <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
     </ol>
     <div class="carousel-inner">
-        <div class="carousel-item active">
+      <div class="carousel-item active">
           <img class="d-block w-100" src="{{asset('picture/image1crs.png')}}" alt="First slide">
       </div>
       <div class="carousel-item">
