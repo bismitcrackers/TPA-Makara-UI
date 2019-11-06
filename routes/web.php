fce5689ee2ea50d7958ad3372d732ee8794f1d22
Unknown <dhafins18@gmail.com>
2019-11-06 21:31:41 +0700

    remove unused route

--- routes/web.php
+++ routes/web.php
@@ -14,22 +14,6 @@
 Route::get('/', 'PageController@index')->name('index');
 Route::get('/success', 'PageController@success')->name('success');
 
-
-Route::get('/editprofilesiswa', 'PageController@editProfileSiswa')->name('editProfileSiswa');
-Route::get('/editprofileibu', 'PageController@editProfileIbu')->name('editProfileIbu');
-Route::get('/editprofileayah', 'PageController@editProfileAyah')->name('editProfileAyah');
-Route::get('/pengumumankegiatan', 'PageController@pengumumanKegiatan')->name('pengumumanKegiatan');
-Route::get('/ubahpengumuman', 'PageController@ubahPengumuman')->name('ubahPengumuman');
-Route::get('/showpengumuman', 'PageController@showPengumuman')->name('showPengumuman');
-Route::get('/jadwalperbulan', 'PageController@jadwalPerBulan')->name('jadwalPerBulan');
-Route::get('/tambahjadwalperbulan', 'PageController@tambahJadwalPerBulan')->name('tambahJadwalPerBulan');
-
-
-Route::get('/tagihansiswa', 'PageController@tagihanSiswa')->name('tagihansiswa');
-Route::get('/tambahtagihan', 'PageController@addTagihan')->name('addTagihan');
-Route::get('/kwitansi', 'PageController@kwitansi')->name('kwitansi');
-Route::get('/notification', 'PageController@notification')->name('notification');
-
 Auth::routes();
 
 Route::group(['prefix' => 'dailyBook', 'as' => 'dailyBook.'], function () {
